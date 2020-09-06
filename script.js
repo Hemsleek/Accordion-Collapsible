ddaef37d948988a9304fdfc7ae28f742bcc3cb9f
hemsleek <asiyanbimubashir@gmail.com>
2020-09-06 23:48:08 +0100

    fix: media

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ $(document).ready(function() {
 
         $(".container").html(`<h1>${headerText}</h1><p>loremsfjskfbjfhfhfg</p>
         <p id="1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere veniam ab labore tempora, cumque adipisci nam error fugit sequi iusto, delectus assumenda animi quis a voluptas aut laudantium repellendus ratione?</p>
-        <p id="2">this a second lorem <br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere veniam ab labore tempora, cumque adipisci nam error fugit sequi iusto, delectus assumenda animi quis a voluptas aut laudantium repellendus ratione?</p>`);
+        <p id="2">this a second lorem <br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere veniam ab labore tempora, cumque adipisci nam error fugit sequi iusto, delectus assumenda animi quis a voluptas aut laudantium repellendus ratione?</p>
+        <p id="3">this a third lorem <br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere veniam ab labore tempora, cumque adipisci nam error fugit sequi iusto, delectus assumenda animi quis a voluptas aut laudantium repellendus ratione</p>
+        <p id="4">this a fourth lorem <br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere veniam ab labore tempora, cumque adipisci nam error fugit sequi iusto, delectus assumenda animi quis a voluptas aut laudantium repellendus ratione</p>`);
 
     });
 });
